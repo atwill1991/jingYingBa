4da19423117eedfa33ca6b52fb38574c187e5153
atwill1991 <atwill1991@163.com>
2016-07-28 14:50:05 +0800

    login

---
+++ html/login/checkCode.php
@@ -0,0 +1,12 @@
+<?php 
+	session_start();
+	$backCode = $_SESSION["check_code_number"];
+	$frontCode = $_GET["code"];
+
+	if($frontCode == $backCode) {
+		echo '{"msg":"1"}';
+	}else {
+		echo '{"msg":"0"}';
+	}
+	// echo $code;
+?>
